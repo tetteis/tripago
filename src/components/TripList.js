f080d210a577d8f9af2f4bffbd0ede3713045a4b
Tettei Shahday-Annang <tetteis@gmail.com>
2022-04-22 08:09:59 +0000

    modified App and TripList components

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,15 +1,36 @@
 import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+import "./TripList.css";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
-  console.log(trips);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
 
-  fetch("http://localhost:3000/trips").then((response) => response.json());
-  // .then((data) => setTrips(data));
+  const { data: trips, isPending, error } = useFetch(url, { type: "GET" });
 
   return (
-    <div>
+    <div className="trip-list">
       <h2>TripList</h2>
+      {isPending && <div>Loading trips...</div>}
+      {error && <div>{error}</div>}
+      <ul>
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
+      </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useFetch = (url, _options) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  //use useRef to wrap an object/array argument
+  //which is a useEffect dependency
+  const options = useRef(_options).current;
+
+  useEffect(() => {
+    console.log(options);
+    const controller = new AbortController();
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const response = await fetch(url, { signal: controller.signal });
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const json = await response.json();
+
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+        }
+      }
+    };
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url, options]);
+
+  return { data, isPending, error };
+};
